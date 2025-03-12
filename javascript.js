b2c652ce54e20b9c62638c8b00ae841d53f46e50
ws <93690318+ws-9@users.noreply.github.com>
2025-03-12 03:45:58 -0400

    big refactor

--- javascript.js
+++ javascript.js
@@ -1,5 +1,40 @@
-const gameController = GameController();
+playGame();
 
+function playGame() {
+  const gameController = GameController();
+
+  let gameState = gameController.getGameState();
+  while (gameState.gameStatus === 'running') {
+    printBoard();
+    console.log(`It is currently ${gameState.currentPlayer}'s turn!`);
+    
+    let selectedIndex = prompt('Choose a slot:');
+    while (!gameController.playRound(selectedIndex)) {
+      selectedIndex = prompt('Try again:');
+    }
+    gameState = gameController.getGameState();
+  }
+  printBoard();
+  switch (gameState.gameStatus) {
+    case 'win':
+      console.log(`${gameState.currentPlayer} won!`);
+      break;
+    case 'tie':
+      console.log('It\'s a tie!');
+      break;
+    default:
+      break;
+  }
+
+  function printBoard() {
+    const board = gameController.getGameState().currentBoard;
+    console.log(
+        `${board[0] || 0} ${board[1] || 1} ${board[2] || 2}\n` +
+        `${board[3] || 3} ${board[4] || 4} ${board[5] || 5}\n` +
+        `${board[6] || 6} ${board[7] || 7} ${board[8] || 8}`
+    );
+  }
+}
 
 function Gameboard() {
   const board = Array(9).fill(null);
@@ -12,81 +47,45 @@ function Gameboard() {
     return false;
   }
 
-  function printBoard() {
-    console.log(
-        `${board[0] || 0} ${board[1] || 1} ${board[2] || 2}\n` +
-        `${board[3] || 3} ${board[4] || 4} ${board[5] || 5}\n` +
-        `${board[6] || 6} ${board[7] || 7} ${board[8] || 8}`
-    );
-  }
-
   function getBoard() {
     return [...board];
   }
-
-  function checkWin(index, { token }) {
-    if (index > 9 || index < 0) {
-      return null;
+  
+  function checkWin({ token }) {
+    const threeInARow = token.repeat(3);
+    for (let i = 0; i < 3; i += 3) {
+      const rowHasNoEmptySlots = board[i] && board[i + 1] && board[i + 2];
+      const rowIsFilledWithSameToken = (board[i] + board[i + 1] + board[i + 2]) === threeInARow;
+      if (rowHasNoEmptySlots && rowIsFilledWithSameToken) {
+        return true;
+      }
     }
-    if (checkDiagonals(index, token) || checkStraights(index, token)) {
+    for (let i = 0; i < 3; ++i) {
+      const colHasNoEmptySlots = board[i] && board[i + 3] && board[i + 6];
+      const colIsFilledWithSameToken = (board[i] + board[i + 3] + board[i + 6]) === threeInARow;
+      if (colHasNoEmptySlots && colIsFilledWithSameToken) {
+        return true;
+      }
+    }
+    if (board[0] && board[4] && board[8] && (board[0] + board[4] + board[8]) === threeInARow) {
       return true;
     }
-    return false
-  }
 
-  function checkDiagonals(index, token) {
-    const tokenLine = token + token + token;
-    if (index % 2 === 1) {
-      return false;
-    }
-    let isFallingDiagonal = (tokenLine === board[0] + board[4] + board[8]);
-    let isRisingDiagonal = (tokenLine === board[2] + board[4] + board[6]);
-
-    switch (index) {
-      case 0:
-      case 8:
-        return isFallingDiagonal;
-      case 2:
-      case 6:
-        return isRisingDiagonal;
-      case 4:
-        return isFallingDiagonal || isRisingDiagonal;
-    }
-  }
-
-  function checkStraights(index, token) {
-    const tokenLine = token + token + token;
-    let vertical = false;
-    let horizontal = false;
-
-    if ([0, 1, 2].includes(index)) {
-      vertical = (tokenLine === board[index] + board[index + 3] + board[index + 6]);
-    } else if ([3, 4, 5].includes(index)) {
-      vertical = (tokenLine === board[index] + board[index - 3] + board[index + 3]);
-    } else if ([6, 7, 8].includes(index)) {
-      vertical = (tokenLine === board[index] + board[index - 3] + board[index - 6]);
+    if (board[2] && board[4] && board[6] && (board[2] + board[4] + board[6]) === threeInARow) {
+      return true;
     }
 
-    if ([0, 3, 6].includes(index)) {
-      horizontal = (tokenLine === board[index] + board[index + 1] + board[index + 2]);
-    } else if ([1, 4, 7].includes(index)) {
-      horizontal = (tokenLine === board[index] + board[index - 1] + board[index + 1]);
-    } else if ([2, 5, 8].includes(index)) {
-      horizontal = (tokenLine === board[index] + board[index - 1] + board[index - 2]);
-    }
-
-    return vertical || horizontal;
+    return false;
   }
 
   return {
     addMark,
     getBoard,
-    printBoard,
     checkWin,
   };
 }
 
-function GameController(player1 = "P1", player2 = "P2") {
+function GameController(player1 = 'P1', player2 = 'P2') {
   let gameStatus = 'running';
   const board = Gameboard();
   const players = [
@@ -101,38 +100,35 @@ function GameController(player1 = "P1", player2 = "P2") {
   ];
 
   let currentPlayer = players[0];
-  console.log(`It is ${currentPlayer.name}'s turn\n`);
-  board.printBoard();
 
-  function getBoard() {
-    return board.getBoard();
+  function getGameState() {
+    return {
+      gameStatus,
+      currentPlayer: currentPlayer.name,
+      currentBoard: board.getBoard(),
+    };
   }
   
-  function switchPlayers() {
-    currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0];
-  }
-
   function playRound(index) {
     if (gameStatus === 'running' && board.addMark(index, currentPlayer)) {
-      const boardIsFull = !getBoard().includes(null)
-      if (board.checkWin(index, currentPlayer)) {
+      const boardIsFull = !board.getBoard().includes(null);
+      const playerHasWon = board.checkWin(currentPlayer);
+
+      if (playerHasWon) {
         gameStatus = 'win';
       } else if (boardIsFull) {
         gameStatus = 'tie';
       } else {
-        switchPlayers();
-        console.log(`It is ${currentPlayer.name}'s turn\n`);
+        currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0];
       }
+      return true;
     }
-    return {
-      gameState: gameStatus,
-      currentPlayer: currentPlayer.name,
-    }
+    return false;
   }
 
   return {
     playRound,
-    getBoard,
+    getGameState,
   }
 }
 
